50d734ed98a5f362cd2437d7c02d4c12da6ffd22
Beaz55455 <156677748+Beaz55455@users.noreply.github.com>
2024-01-19 21:10:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['0655459181', 'BEAZ', true],
+  ['0655459181', 'Ash', true], 
   [''] 
 ] //Number of owners
 
